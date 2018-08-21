8a3c1fd519dbf12c2181cfc173ddce5abc447b87
pablocordeiro <pabcordeiro@gmail.com>
2018-08-21 16:44:28 -0300

    Criando cadastros e pesquisas de funcionarios e veiculos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function() {
 	$('[data-toggle="tooltip"').tooltip();
+
+	$('.datepicker').datepicker({
+	    format: 'dd/mm/yyyy',
+	    language: 'pt-BR',
+	    autoclose: true
+	});	
 });
